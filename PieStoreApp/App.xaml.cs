9cff2dfdf4e386681cd4dd00273738d3314dc7d7
hkirfan <hkirfan@hsc.wvu.edu>
2021-01-31 17:46:43 -0500

    Add project files.

---
+++ PieStoreApp/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PieStoreApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            bool alreadyRegistered = false;
+            //MainPage = new LoginView();
+            //MainPage = new RegisterView();
+            //MainPage = new ContactView();
+            //MainPage = new SettingsView();
+            //MainPage = new WebPageView();
+            //MainPage = new PieDetailView(); 
+            //MainPage = new PieOverview();
+
+            if (alreadyRegistered)
+                MainPage = new MainView();
+            else
+                MainPage = new NavigationPage(new LoginView());
+
+
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ PieStoreApp/MainView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PieStoreApp.Model;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PieStoreApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainView : MasterDetailPage
+	{
+		public MainView ()
+		{
+			InitializeComponent ();
+
+		    MasterView.NavigationListView.ItemSelected += NavigationListView_ItemSelected;
+        }
+
+	    private void NavigationListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+	    {
+	        if (e.SelectedItem is MasterNavigationItem item)
+	        {
+	            Detail = new NavigationPage((Page)Activator.CreateInstance(item.Target));
+	            MasterView.NavigationListView.SelectedItem = null;
+	            IsPresented = false;
+	        }
+	    }
+    }
+}
